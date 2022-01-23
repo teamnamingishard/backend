95693e48cd6fc13068dfd856eb663c49d9a6fc42
SeungWookHan <hswook12@me.com>
2022-01-23 16:25:40 +0900

    Initial Commit

---
+++ api/src/test/java/com/spystock/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spystock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
